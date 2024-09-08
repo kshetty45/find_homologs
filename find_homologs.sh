10626b2839ba725f53e4210d8257019731deb165
kshetty45 <kshetty45@gatech.edu>
2024-09-08 15:53:45 -0400

    Filter hits to keep only hits with >30% sequence identity and >90% match length (90% of the query sequence length)

--- find_homologs.sh
+++ find_homologs.sh
@@ -1,12 +1,20 @@
+#!/bin/bash
 
 if [ "$#" != 3 ]; then
-	echo "Warning: Must have 1 QUERY file, 1 SUBJECT file, and 1 OUTPUT file"
-	exit 1
+    echo "Warning: Must have 1 QUERY file, 1 SUBJECT file, and 1 OUTPUT 
+file"
+    exit 1
 fi
 
-blastn -query "$1" -subject "$2" -task blastn-short -outfmt '6 qseqid sseqid pident length qlen slen' -out basefile.txt
 
-awk '$3 == 100 && $4==$5' basefile.txt > "$3"
+
+tblastn -query "$1" -subject "$2" -task blastn-short -outfmt '6 qseqid 
+sseqid pident length qlen slen' -out basefile.txt
+
+match_length_factor=0.90
+
+awk -v match_length_factor="$match_length_factor" '$3 > 30 && $4 > 
+match_length_factor * $5' basefile.txt > "$3"
 
 Matches=$(cat "$3" | wc -l)
 echo "Number of Matches: $Matches"
